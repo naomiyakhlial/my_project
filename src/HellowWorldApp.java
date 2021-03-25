90dffa4e91e771641d921769a161419d0cbf80ad
naomi <naomiya@campus.technion.ac.il>
2021-03-25 14:45:14 +0200

    add print

---
+++ src/HellowWorldApp.java
@@ -0,0 +1,5 @@
+public class HellowWorldApp {
+    public static void main (String[] args) {
+        System.out.print("hello world");
+    }
+}
--- src/my_class.java
+++ src/my_class.java
@@ -2,5 +2,8 @@ public class my_class {
     public static void main(String[] args) {
         int naomi=24;
         int roy= 24;
+        System.out.println("naomi is the best");
+        System.out.print(naomi);
+
     }
 }
